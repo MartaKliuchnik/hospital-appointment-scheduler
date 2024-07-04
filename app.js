e574adb80ef24608cc8791dc4850342feb199cdc
Marta <martasagun95@gmail.com>
2024-07-04 12:16:06 +0200

    feat: add template for registration and login processes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const rootDir = require('./utils/path');
 const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
@@ -10,7 +12,15 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json()); // postman
-app.use('/', authRoutes); // OAuth
+app.use(bodyParser.urlencoded({ extended: true })); // browser
+app.use(
+	session({
+		secret: 'bde60e6a8b6348c1c0e98d2f2', // change with crypto
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use('/auth', authRoutes);
 app.use('/appointments', appointmentRoutes);
 
 app.get('/welcome-page', (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,12 @@ const authController = require('../controllers/auth');
 
 const router = Router();
 
-router.get('/login', authController.getOAuth);
-router.get('/google/callback', authController.getGoogleCallback);
+router.get('/login', authController.getLogin);
+
+router.post('/login', authController.postLogin);
+
+router.get('/register', authController.getRegister);
+
+router.post('/register', authController.postRegister);
 
 module.exports = router;
